36102f0010d25487c4851c34979a4dc361955900
Michael Sam <sam.michael.h@gmail.com>
2024-03-29 18:22:30 +0900

    Add playGame and playerChoice functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const options = ["Rock", "Paper", "Scissors"];
+const options = ["rock", "paper", "scissors"];
 
 function getComputerChoice () {
   const choice = options[Math.floor(Math.random() * options.length)];
@@ -10,9 +10,9 @@ function getComputerChoice () {
 //     return "It's a tie!";
 //   }
 //   else if (
-//     (playerSelection == "Rock" && computerSelection == "Scissors") ||
-//     (playerSelection == "Paper" && computerSelection == "Rock") ||
-//     (playerSelection == "Scissors" && computerSelection == "Paper")
+//     (playerSelection == "rock" && computerSelection == "scissors") ||
+//     (playerSelection == "paper" && computerSelection == "rock") ||
+//     (playerSelection == "scissors" && computerSelection == "paper")
 //   ) {
 //     return `You win! ${playerSelection} beats ${computerSelection}.`;
 //   }
@@ -26,9 +26,9 @@ function playRound(playerSelection, computerSelection) {
     return "It's a tie!";
   }
   else if (
-    (playerSelection == "Rock" && computerSelection == "Scissors") ||
-    (playerSelection == "Paper" && computerSelection == "Rock") ||
-    (playerSelection == "Scissors" && computerSelection == "Paper")
+    (playerSelection == "rock" && computerSelection == "scissors") ||
+    (playerSelection == "paper" && computerSelection == "rock") ||
+    (playerSelection == "scissors" && computerSelection == "paper")
   ) {
     return `You win! ${playerSelection} beats ${computerSelection}.`;
   }
@@ -37,6 +37,29 @@ function playRound(playerSelection, computerSelection) {
   };
 };
 
-const playerSelection = "Rock";
-const computerSelection = getComputerChoice();
-console.log(playRound(playerSelection, computerSelection));
+function playerChoice() {
+  let validatedInput = false;
+  while (validatedInput == false) {
+    const choice = prompt("Rock, Paper, or Scissors");
+    if (choice == null) {
+      continue;
+    };
+    const choiceInLower = choice.toLowerCase();
+    if (options.includes(choiceInLower)) {
+      validatedInput = true;
+      return choiceInLower;
+    };
+  };
+};
+
+function playGame() {
+  console.log("Welcome!");
+  for (let i = 0; i < 5; i ++) {
+    const playerSelection = playerChoice();
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  };
+  console.log("Game over.");
+};
+
+playGame();
