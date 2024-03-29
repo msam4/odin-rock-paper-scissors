5ec20aec044854393eb9531796313e3846734204
Michael Sam <sam.michael.h@gmail.com>
2024-03-29 17:36:10 +0900

    Add getComputerChoice and playRound functions. Created checkWinner function but it is not required

--- script.js
+++ script.js
@@ -1,7 +1,42 @@
-const choice = ["Rock", "Paper", "Scissors"];
+const options = ["Rock", "Paper", "Scissors"];
 
 function getComputerChoice () {
-  choice.random;
-}
+  const choice = options[Math.floor(Math.random() * options.length)];
+  return choice;
+};
 
-getComputerChoice();
+// function checkWinner(playerSelection, computerSelection) {
+//   if (playerSelection == computerSelection) {
+//     return "It's a tie!";
+//   }
+//   else if (
+//     (playerSelection == "Rock" && computerSelection == "Scissors") ||
+//     (playerSelection == "Paper" && computerSelection == "Rock") ||
+//     (playerSelection == "Scissors" && computerSelection == "Paper")
+//   ) {
+//     return `You win! ${playerSelection} beats ${computerSelection}.`;
+//   }
+//   else {
+//     return `You lose... ${computerSelection} beats ${playerSelection}.`;
+//   };
+// };
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection == computerSelection) {
+    return "It's a tie!";
+  }
+  else if (
+    (playerSelection == "Rock" && computerSelection == "Scissors") ||
+    (playerSelection == "Paper" && computerSelection == "Rock") ||
+    (playerSelection == "Scissors" && computerSelection == "Paper")
+  ) {
+    return `You win! ${playerSelection} beats ${computerSelection}.`;
+  }
+  else {
+    return `You lose... ${computerSelection} beats ${playerSelection}.`;
+  };
+};
+
+const playerSelection = "Rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
