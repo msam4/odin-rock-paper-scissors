cd1ec6beda0ce04933d8ab55a2f9becca7dd9813
Michael Sam <sam.michael.h@gmail.com>
2024-03-29 21:07:51 +0900

    Add score and console log winner/loser message in playGame function. Remove commented out functions

--- script.js
+++ script.js
@@ -1,25 +1,11 @@
 const options = ["rock", "paper", "scissors"];
+let playerScore = 0;
+let computerScore = 0;
 
 function getComputerChoice () {
   const choice = options[Math.floor(Math.random() * options.length)];
   return choice;
 };
-
-// function checkWinner(playerSelection, computerSelection) {
-//   if (playerSelection == computerSelection) {
-//     return "It's a tie!";
-//   }
-//   else if (
-//     (playerSelection == "rock" && computerSelection == "scissors") ||
-//     (playerSelection == "paper" && computerSelection == "rock") ||
-//     (playerSelection == "scissors" && computerSelection == "paper")
-//   ) {
-//     return `You win! ${playerSelection} beats ${computerSelection}.`;
-//   }
-//   else {
-//     return `You lose... ${computerSelection} beats ${playerSelection}.`;
-//   };
-// };
 
 function playRound(playerSelection, computerSelection) {
   if (playerSelection == computerSelection) {
@@ -58,8 +44,15 @@ function playGame() {
     const playerSelection = playerChoice();
     const computerSelection = getComputerChoice();
     console.log(playRound(playerSelection, computerSelection));
+    console.log("------------");
   };
   console.log("Game over.");
+  if (playerScore > computerScore) {
+    console.log("You are the winner!");
+  }
+  else {
+    console.log("You lose to the computer...");
+  }
 };
 
 playGame();
